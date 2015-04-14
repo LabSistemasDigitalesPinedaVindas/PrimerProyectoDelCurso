659ef9b204d7b60010c1386d4b0817efad3a7e8b
Andres29 <andrespimo@gmail.com>
2015-04-13 20:42:45 -0600

    Sumador con deteccion de overflow
    Falta la detecccion de underflow

--- Sumador.v
+++ Sumador.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date:    10:20:33 04/11/2015 
+// Create Date:    13:28:39 04/13/2015 
 // Design Name: 
 // Module Name:    Sumador 
 // Project Name: 
@@ -18,30 +18,35 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Sumador #(parameter Width = 16, Signo = 1, Magnitud = 3, Presicion = 12)// Parametros por senal
-							(A, B, Y);					   //Entradas y salida
-							
-		input signed [Width-1:0] A,B;			//Definir entradas
-		output reg signed [Width-1:0] Y; 	//Define salidas						
-		wire [Width-1:0] Aux; 				//Variable auxiliar para realizar calculos	
-		//Aux == A+B; 	REVISAR								//Operación de suma
+module Sumador#(parameter Width = 4, Signo = 1, Magnitud = 1, Presicion = 2)// Parametros por senal
+							(A, B, Y, N);					   //Entradas y salida
+		 
+		input [Width-1:0] A,B;			//Definir entradas
+		output reg [Width-1:0] Y; 	//Define salidas						
+		reg signed [Width-1:0] Aux; 			//Variable auxiliar para realizar calculos
+		output reg N;
+		//assign Aux = A+B; 						//Operación de suma
 		
-		always  
-			begin
-				if (~A[Width-1] & ~B[Width-1] & Aux[Width-1]) //Verificación de overflow & underflow
-					Y = 2**((Width-1)-1); 
+		always @*
+			Aux = A+B;
 			
-	//			else if 
-	//			//agregar más condiciones 
-	//			else if 
-	//			//agregar más condiciones 
-	//			else if 
-	//			//agregar más condiciones 
-	//			else if 
-	//			//agregar más condiciones 
-//			
+		always @*
+			begin
+			 	if (~A[Width-1] && ~B[Width-1] && Aux[Width-1])     //Verificación de overflow 
+				begin	
+					//Y= 4'b1111;
+					Y = (2**(Width-1))-1;
+					N = 0; end
+			
+				else if (A[Width-1] && B[Width-1] && ~Aux[Width-1]) //Verificación de overflow 
+					begin 
+					//Y=4'b1111;
+					Y=(2**(Width-1))+1;
+					N = 1; end 
+						
 				else 
-					Y = Aux[Width-1];
+				Y = Aux;
+				N=2;
+				
 			end
-		
 endmodule
