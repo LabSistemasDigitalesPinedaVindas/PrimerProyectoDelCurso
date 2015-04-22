4739a8457c692f34dca2cfc0866fff474f0ef009
Andres29 <andrespimo@gmail.com>
2015-04-21 19:45:26 -0600

    Sumador
    Deteccion de overflow

--- Sumador.v
+++ Sumador.v
@@ -18,35 +18,45 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Sumador#(parameter Width = 4, Signo = 1, Magnitud = 1, Presicion = 2)// Parametros por senal
-							(A, B, Y, N);					   //Entradas y salida
+module Sumador#(parameter Width = 25, Signo = 1, Magnitud = 8, Presicion = 16)// Parametros por senal
+							(A, B, Y);					   //Entradas y salida
 		 
 		input [Width-1:0] A,B;			//Definir entradas
 		output reg [Width-1:0] Y; 	//Define salidas						
 		reg signed [Width-1:0] Aux; 			//Variable auxiliar para realizar calculos
-		output reg N;
+		reg signed [Width-1:0] maximo;
+		reg signed [Width-1:0] minimo;
+		
+		
+		
 		//assign Aux = A+B; 						//Operación de suma
 		
 		always @*
+			begin
 			Aux = A+B;
+			maximo = (2**(Width-1))-1;
+			minimo = (2**(Width-1))+1;
+			end
 			
 		always @*
 			begin
 			 	if (~A[Width-1] && ~B[Width-1] && Aux[Width-1])     //Verificación de overflow 
 				begin	
 					//Y= 4'b1111;
-					Y = (2**(Width-1))-1;
-					N = 0; end
+					//Y = (2**(Width-1))-1;
+					Y = maximo;
+				 end
 			
 				else if (A[Width-1] && B[Width-1] && ~Aux[Width-1]) //Verificación de overflow 
 					begin 
 					//Y=4'b1111;
-					Y=(2**(Width-1))+1;
-					N = 1; end 
+					//Y=(2**(Width-1))+1;
+					Y = minimo;
+				 end 
 						
-				else 
+				else
+				begin
 				Y = Aux;
-				N=2;
-				
+				end
 			end
 endmodule
