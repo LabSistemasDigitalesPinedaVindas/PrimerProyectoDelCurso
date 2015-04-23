5bdba5d6153bd7d469f3a6878269b9628db40f23
Andres29 <andrespimo@gmail.com>
2015-04-22 18:40:11 -0600

    Matlab
    Filtros, Conversion, Creacion de datos aleatorios en string

---
+++ PasoBajo25kHz.m
@@ -0,0 +1,23 @@
+%filtro de orden 2 paso bajo
+%frecuencia de corte 25kHz
+function [y] = PasoBajo25kHz (Entrada)
+
+fNMenos1 = 0;
+fNMenos2 = 0;
+
+a1 = 1.591;
+a2 = 0.6617;
+b0 = 0.8132;
+b1 = 1.626;
+b2 = 0.8132;
+
+n = length(Entrada);
+y = [];
+for i=1:1:n
+    f=Entrada(i)-a1*fNMenos1-a2*fNMenos2;
+    y(i)=b0*f+b1*fNMenos1+b2*fNMenos2;
+    fNMenos2=fNMenos1;
+    fNMenos1=f;
+end
+
+
